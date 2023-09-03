d8ee47cfe1e2e331d30d3acd4e0718107cac3bcc
Bob Moriasi <official.bobmoriasi@gmail.com>
2023-09-04 00:02:52 +0300

    Initial Commit

---
+++ NSPasteboard/NSPasteboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  NSPasteboardApp.swift
+//  NSPasteboard
+//
+//  Created by Bob Moriasi on 9/4/23.
+//
+
+import SwiftUI
+
+@main
+struct NSPasteboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
